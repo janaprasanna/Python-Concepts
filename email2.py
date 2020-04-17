b7d7c30f564367110256ef7642377cc1be73f6bc
janaprasanna <emailtojana27@gmail.com>
2020-04-17 19:32:57 +0530

    doubt where # symbol der

---
+++ email2.py
@@ -0,0 +1,13 @@
+def jana1():
+    fn = str(input("enter your first name:"))
+    ln = str(input("enter your last name:"))
+
+    print("your first name is {0}".format(fn))
+    print("your last name is {0}".format(ln))
+
+#def genmail():
+
+    print("your generated email id is {0}.{1}@gmail.com".format(fn,ln))
+
+jana1()
+#genmail()
