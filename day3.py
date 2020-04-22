6a692e0fec1e272f3c901630ccc0dc06a4081b8b
janaprasanna <emailtojana27@gmail.com>
2020-04-22 10:08:04 +0530

    PRINTING PATTERNS

--- day3.py
+++ day3.py
@@ -1,1 +1,13 @@
-print("helloworld")
+str='*****'
+j = 5
+for i in str:  # Using for loop
+    print(str[:j])
+
+    j-=1
+
+
+str = '*'
+i=5
+while i>=1:  #using while loop
+    print(str*i)
+    i-=1
